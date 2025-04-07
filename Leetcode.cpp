bae7807f70692a0997f12d53d0f9db78ed49877c
Crist Yang <cr1st4ever@outlook.com>
2025-04-07 23:03:58 +0800

    416. 分割等和子集

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<numeric>
 using namespace std;
 
 /*
@@ -163,6 +164,46 @@ answer[j] % answer[i] == 0
 		reverse(result.begin(), result.end());
 		return result;
 	}
+    /*
+    416. 分割等和子集
+    */
+    bool canPartition(vector<int>& nums) {
+        int i = 0;
+        for (unsigned int i : nums)
+        {
+            int n = nums.size();
+            if (n < 2) {
+                return false;
+            }
+            int sum = accumulate(nums.begin(), nums.end(), 0);
+            int maxNum = *max_element(nums.begin(), nums.end());
+            if (sum & 1) {
+                return false;
+            }
+            int target = sum / 2;
+            if (maxNum > target) {
+                return false;
+            }
+            vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+            for (int i = 0; i < n; i++) {
+                dp[i][0] = true;
+            }
+            dp[0][nums[0]] = true;
+            for (int i = 1; i < n; i++) {
+                int num = nums[i];
+                for (int j = 1; j <= target; j++) {
+                    if (j >= num) {
+                        dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                    }
+                    else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+            return dp[n - 1][target];
+        }
+
+    }
 };
 
 int main(void) {
